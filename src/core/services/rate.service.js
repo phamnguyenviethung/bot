5bc378af1a87fa720ec3e44a34c24fa1dea4058c
Viet Hung <viethung01234@gmail.com>
2024-10-21 10:16:43 +0700

    fix calc main rate

--- src/core/services/rate.service.js
+++ src/core/services/rate.service.js
@@ -30,19 +30,19 @@ class RateService {
     } else if (totalMoney > 1 * 1000000000) {
       return rateConstant.HIGH;
     } else {
-      return totalCoin >= 100000000 ? rateConstant.MEDIUM : rateConstant.LOW;
+      return totalCoin > 10000 ? rateConstant.MEDIUM : rateConstant.LOW;
     }
   };
 
   calcCoinPrice = async (mainRate) => {
     let coinBuyPrice = _.random(2000000, 5000000) * (mainRate + 2);
     if ([rateConstant.LOW, rateConstant.MEDIUM].includes(mainRate)) {
-      coinBuyPrice = _.random(20000, 40000) * (mainRate - 1);
+      coinBuyPrice = _.random(200000, 6000000) * mainRate;
     } else if (rateConstant.HIGH === mainRate) {
-      coinBuyPrice = _.random(500000, 2000000) * (mainRate + 1);
+      coinBuyPrice = _.random(800000, 3000000) * (mainRate + 1);
     }
 
-    const PERCENT = _.random(45, 90) / 100;
+    const PERCENT = _.random(45, 80) / 100;
 
     return {
       coinBuyPrice,
