c5eecd05d8e171e38870fc5b25331a2b89661ece
Viet Hung <viethung01234@gmail.com>
2025-01-23 07:21:31 +0700

    fix redis client

--- src/configs/redis.config.js
+++ src/configs/redis.config.js
@@ -1,14 +1,15 @@
 // redisClient.js
 const Redis = require('ioredis');
 const { logger } = require('./logger.config');
+const botConfig = require('./bot.config');
 
 // Sử dụng cấu hình từ môi trường
 const redis = new Redis({
-  port: 6379,
-  host: 'localhost',
-  username: 'default',
-  password: '1234',
-  db: 2, // Defaults to 0
+  port: botConfig.redis.port,
+  host: botConfig.redis.host,
+  username: botConfig.redis.username,
+  password: botConfig.redis.password,
+  db: botConfig.redis.db, // Defaults to 0
 });
 
 redis.on('error', (err) => logger.error('Redis connection error:', err));
